9590aad766ef4ede3cc3d70964492412cf52f426
aarondud <aarondudley00@gmail.com>
2024-01-26 16:47:31 +0800

    configure to notion

--- site.config.ts
+++ site.config.ts
@@ -2,31 +2,31 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'c0163542ce7c409e8e6743a232dec375',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Aaron Dudley',
+  domain: 'temp',
+  author: 'Aaron Dudley',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: "Aaron Dudley's personal portfolio & website",
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  github: 'aarondud',
+  linkedin: 'aaron-dudley',
+  newsletter: 'mailto:aarondudley00@gmail.com',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: null,
+  defaultPageIcon: null, 
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
@@ -50,16 +50,20 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages. To use `navigationLinks`, set `navigationStyle` to `custom`.
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  //navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Portolio',
+      pageId: '85add585283243f1895dc9e58e97635e'
+    },
+    {
+      title: 'Blog',
+      pageId: '792b81c8c05942d89fbfa1d4872fc533'
+    },
+    {
+      title: 'Experience',
+      pageId: '7123ac93d7de4dbba26ea3fe797fd0bc?pvs=4'
+    },
+  ]
 })
